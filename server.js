519d2627c503501f0b04c0f4d5d0d3413abee47a
Hari Ram <tmhariram@gmail.com>
2025-12-31 00:14:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("Mongo Error:", err));
 
