d1bda84c8c1a97d01c6d3ad49265fd79ccdcbf71
Hari Ram <tmhariram@gmail.com>
2025-12-31 00:10:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,21 +15,20 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo Error:", err));
+
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/leads",leadRoutes);
+app.use("/api/leads", leadRoutes);
 app.use("/api/employee", employeesRoutes);
 
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
